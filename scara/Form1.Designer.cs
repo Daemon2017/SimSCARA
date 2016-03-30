11f404c1d633db0c7aecdc38a778e0968d00c376
Dmitrij Rusakov <Daemon2017@gmail.com>
2016-03-31 00:10:59 +0300

    1st

---
+++ scara/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace scara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(669, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Обратная задача:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(669, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "O2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(669, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "O3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(772, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Прямая задача:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(771, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(772, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Y";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обратная";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(669, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "X";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(669, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Y";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(721, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "a2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(721, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "a3";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(772, 55);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "O3";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(772, 29);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 14;
+            this.textBox8.Text = "O2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(772, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Прямая";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(772, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Чертить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(772, 219);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(736, 248);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Раб. зона";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "SCARA SIM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ scara/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double O2, O3, O, x0, y0, x1, y1, x01, y01;
+        double a2, a3, x, y;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x0 = (pictureBox1.Size.Width / 2);
+            y0 = (pictureBox1.Size.Height / 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            a2 = double.Parse(textBox3.Text);
+            a3 = double.Parse(textBox4.Text);
+
+            O2 = Math.Atan(y / x) - Math.Asin((a3 / Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2))) * Math.Sqrt(1 - Math.Pow(((Math.Pow(x, 2) + Math.Pow(y, 2) - Math.Pow(a2, 2) - Math.Pow(a3, 2)) / (2 * a2 * a3)), 2)));
+            O3 = Math.Acos((Math.Pow(x, 2) + Math.Pow(y, 2) - Math.Pow(a2, 2) - Math.Pow(a3, 2)) / (2 * a2 * a3));
+
+            label2.Text = O2.ToString();
+            label3.Text = O3.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            O2 = double.Parse(textBox8.Text);
+            O3 = double.Parse(textBox7.Text);
+            a2 = double.Parse(textBox3.Text);
+            a3 = double.Parse(textBox4.Text);
+
+            x1 = a3 * Math.Cos(O2 + O3) + a2 * Math.Cos(O2);
+            y1 = a3 * Math.Sin(O2 + O3) + a2 * Math.Sin(O2);
+
+            label5.Text = x1.ToString();
+            label6.Text = y1.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics graph = pictureBox1.CreateGraphics();
+
+            Pen pen_black = new Pen(Brushes.Black, 4);
+            Pen pen_red = new Pen(Brushes.Red, 3);
+
+            x01 = x0 + a2 * Math.Cos(O2);
+            y01 = y0 + a2 * Math.Sin(O2);
+
+            graph.DrawLine(pen_red, (float)x0, (float)y0, (float)x01, (float)y01);
+            graph.DrawLine(pen_red, (float)x01, (float)y01, (float)(x0 + x1), (float)(y0 + y1));
+
+            graph.DrawEllipse(pen_black, (float)x0, (float)y0, 1, 1);
+            graph.DrawEllipse(pen_black, (float)x01, (float)y01, 1, 1);
+            graph.DrawEllipse(pen_black, (float)(x0 + x1), (float)(y0 + y1), 1, 1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                O = O3;
+                O3 = O3 + 0.1;
+
+                if (O3 != O)
+                {
+                    O3 = O3 + 0.01;
+                    O2 = O2 + 0.01;
+
+                    Graphics graph = pictureBox1.CreateGraphics();
+
+                    Pen pen_black = new Pen(Brushes.Black, 4);
+                    Pen pen_red = new Pen(Brushes.Red, 3);
+
+                    x01 = x0 + a2 * Math.Cos(O2);
+                    y01 = y0 + a2 * Math.Sin(O2);
+
+                    x1 = a3 * Math.Cos(O2 + O3) + a2 * Math.Cos(O2);
+                    y1 = a3 * Math.Sin(O2 + O3) + a2 * Math.Sin(O2);
+
+                    graph.DrawLine(pen_red, (float)x0, (float)y0, (float)x01, (float)y01);
+                    graph.DrawLine(pen_red, (float)x01, (float)y01, (float)(x0 + x1), (float)(y0 + y1));
+
+                    graph.DrawEllipse(pen_black, (float)x0, (float)y0, 1, 1);
+                    graph.DrawEllipse(pen_black, (float)x01, (float)y01, 1, 1);
+                    graph.DrawEllipse(pen_black, (float)(x0 + x1), (float)(y0 + y1), 1, 1);
+                }
+                else
+                {
+                    timer1.Stop();
+                }
+        }
+    }
+}
